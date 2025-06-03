5f6472ca48841f2014cf40abc8060528f3cb9cea
Jays017 <jsgit017@gmail.com>
2025-06-03 14:46:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+// Data Types in JavaScript
+
+// alert(3+3); we are using node js , not browser 
+
+console.log(3 + 3) // code readability 
+console.log("Hello World!")
+
+
+let name =" Jayesh"
+let age = 19
+let isLoggedIn = false 
+let state;
+
+// number => 2 to power 53
+// bigint use for large numbers eg in stock market 
+// string  => "" (string uses double quote )
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol (unique value)
+
+
+
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined
