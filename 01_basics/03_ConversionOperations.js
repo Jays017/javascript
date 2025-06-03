39a0fdfb811bfabc88f97800f6ad364b6bebc79d
Jays017 <jsgit017@gmail.com>
2025-06-03 15:01:58 +0000

    Conversiton Operations of datatypes in JavaScript

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,26 @@
+let score = "Jayesh"
+
+//console.log(typeof score) ;
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber) ;
+//console.log( valueInNumber) ;
+
+// "33" easily converts to number
+// "33ac" converts to NaN
+// true converts to 1 false converts to 0
+
+let isLoggedIn = "Jayesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn) ; 
+
+// 1 converts to true in boolean 
+// "" empty string converts to false
+// "Jayesh" converts to true
+
+let someNumber = 33
+ let stringNumber = String(someNumber);
+ console.log(stringNumber) ;
+ console.log(typeof stringNumber) ;
