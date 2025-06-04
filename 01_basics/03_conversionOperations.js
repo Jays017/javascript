9652ce7859f9074a5a64add499baec1917617b5a
Jays017 <jsgit017@gmail.com>
2025-06-04 15:12:40 +0000

    updates

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,56 @@
+let score = "Jayesh"
+
+//console.log(typeof score) ;
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber) ;
+//console.log( valueInNumber) ;
+
+// "33" easily converts to number
+// "33ac" converts to NaN
+// true converts to 1 false converts to 0
+
+let isLoggedIn = "Jayesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn) ; 
+
+// 1 converts to true in boolean 
+// "" empty string converts to false
+// "Jayesh" converts to true
+
+let someNumber = 33
+ let stringNumber = String(someNumber);
+//  console.log(stringNumber) ;
+//  console.log(typeof stringNumber) ;
+
+ // *******************Operations ********************
+
+ let value = 3
+ let negValue = -value 
+//  console.log(negValue) ;
+
+// console.log(3 + 4)
+// console.log(3** 4)
+// console.log(3% 4)
+let str1 = "hello"
+let str2 = "world"
+
+let str3 = str1+ str2
+// console.log(str3) ; 
+
+// console.log("1" + 2)
+// console.log("1" + 2 +2)
+// console.log(1 + 2 + "2")
+
+// console.log(true)
+// console.log(+"")
+
+let num1,num2,num3
+
+num1=num2=num3=2 + 2
+
+let gameCounter =100
+ gameCounter++;
+ console.log(gameCounter);
